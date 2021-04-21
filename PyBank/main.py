f30e44415ae5939875729df0d3aa3038c1ee639e
dalismo <agonz1975@gmail.com>
2021-04-20 20:15:32 -0400

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,9 @@
 import os
 import csv
+
+#reading the file
+csvpath = os.path.join ('Resources','budget_data.csv')
+
+with open (csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    print (csvreader)
