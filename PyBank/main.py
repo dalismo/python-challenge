30b24cb9a70752bc4adf64412761b34797e3de48
dalismo <agonz1975@gmail.com>
2021-04-21 22:13:14 -0400

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,9 @@
-# help creating filepath
 import os
-
-# module to read csv
 import csv
 
-#variables to storee
-months_count = 0
-total_gainloss = 0
-change_gainloss = 0
+csvpath = os.path.join('..','Resources','budget_data.csv')
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+ 
 
-#loop
-    # where the file is located
-    budgetpath = os.path.join("..","Resources","budget_data.csv")
-
-# Open the CSV
-with open(budgetpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
 
