e455d258ffef9cb12216cda0f4161dace35fd3a0
dalismo <agonz1975@gmail.com>
2021-04-22 19:55:10 -0400

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,8 +2,17 @@
 import csv
 import os
 
+# filepath location
 budget_csv = os.path.join('.','Resources','budget_data.csv')
+
+#check on path
 print(budget_csv)
+
+# opening and reading the file
 with open(budget_csv, 'r') as budget_csv:
     csvreader = csv.reader(budget_csv, delimiter=',')
+
+# lists to contain counts/calcs
+months = []
+gainloss = []
 
