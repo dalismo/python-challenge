3311b96f38d8eebb3f900f8a7945e75cad6165c7
dalismo <agonz1975@gmail.com>
2021-04-22 21:44:06 -0400

    add:

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,9 +5,6 @@ import csv
 # filepath 
 budget_csv = os.path.join('.','Resources','budget_data.csv')
 
-# check on path
-print(budget_csv)
-
 # variables to store for counts, calcs and loop
 total_months = 0
 total_net_gainloss = 0
@@ -15,7 +12,7 @@ greatest_increase_gainloss = 0
 greatest_decrease_gainloss = 0
 total_gainloss = 0
 prior_gainloss = 0
-%_gainloss = 0
+percent_gainloss = 0
 difference = 0
 difference_list = []
 
@@ -34,8 +31,9 @@ with open(budget_csv) as csvfile:
     # start of the loop through the data
     for row in csvreader:
         total_months = total_months + 1
-        total_gainloss = total_gainloss = int(row[1])
-        %_gainloss = int(row[1])
+        total_gainloss = total_gainloss + int(row[1])
+        percent_gainloss = int(row[1])
+        difference = percent_gainloss - prior_gainloss
+        difference_list.append(difference_list)
         
-
 
