4a3297dace2d50d77cee8e64d5e3858ab6bdd41d
dalismo <agonz1975@gmail.com>
2021-04-22 20:29:17 -0400

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,16 @@
-# modules
-import csv
 import os
+import csv
 
-# filepath location
+# filepath 
 budget_csv = os.path.join('.','Resources','budget_data.csv')
 
-#check on path
+# check on path
 print(budget_csv)
 
-# opening and reading the file
-with open(budget_csv, 'r') as budget_csv:
-    csvreader = csv.reader(budget_csv, delimiter=',')
+# variables to store for counts
+total_months = 0
+toal_gainloss = 0
+greatestincrease_gainloss = 0
+greatestdecrease_gainloss = 0
 
-# lists to contain counts/calcs
-months = []
-gainloss = []
-
-# starting that great loop
-for row in csvreader:
-    month.append(row[1])
-    profit.apend(int(row[1])
-    
 
