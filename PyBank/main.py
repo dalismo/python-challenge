e434da2574eaf682b6b2b68d064cac0253d3c3a0
dalismo <agonz1975@gmail.com>
2021-04-22 20:37:49 -0400

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,11 +8,17 @@ budget_csv = os.path.join('.','Resources','budget_data.csv')
 # check on path
 print(budget_csv)
 
-# variables to store for counts
+# variables to store for counts, calcs and loop
 total_months = 0
 toal_gainloss = 0
-greatestincrease_gainloss = 0
-greatestdecrease_gainloss = 0
+greatest_increase_gainloss = 0
+greatest_decrease_gainloss = 0
+total_gainloss = 0
+prior_gainloss = 0
+change_gainloss = 0
+difference = 0
+difference_list = []
+
 
 # Open and read the file using module
 with open(budget_csv, newline="") as csvfile:
