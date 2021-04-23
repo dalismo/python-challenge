b3abb1bd3e68878ff6634de694cad1506f599003
dalismo <agonz1975@gmail.com>
2021-04-23 18:15:18 -0400

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,9 @@ import csv
 # filepath 
 budget_csv = os.path.join('.','Resources','budget_data.csv')
 
+print(budget_csv)
+
+
 # variables to store for counts, calcs and loop
 total_months = 0
 total_net_gainloss = 0
@@ -16,6 +19,9 @@ percent_gainloss = 0
 difference = 0
 difference_list = []
 
+# check on variables
+print(total_months)
+
 # Open and read the file using module
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -23,46 +29,47 @@ with open(budget_csv) as csvfile:
    
     # variable to store rows
     firstrow = next(csvreader)
+    
 
-    # take the prior value from the firstrow
-    prior_gainloss = int(firstrow[1])
+#     # take the prior value from the firstrow
+#     prior_gainloss = int(firstrow[1])
 
-    # start of the loop through the data
-    for row in csvreader:
-        total_months = total_months + 1
-        total_gainloss = total_gainloss + int(row[1])
-        percent_gainloss = int(row[1])
-        difference = percent_gainloss - prior_gainloss
-        difference_list.append(difference_list)
-        prior_gainloss = percent_gainloss
+#     # start of the loop through the data
+#     for row in csvreader:
+#         total_months = total_months + 1
+#         total_gainloss = total_gainloss + int(row[1])
+#         percent_gainloss = int(row[1])
+#         difference = percent_gainloss - prior_gainloss
+#         difference_list.append(difference_list)
+#         prior_gainloss = percent_gainloss
 
-        # conditionals
-        if percent_gainloss > greatest_increase_gainloss:
-            greatest_increase_gainloss = percent_gainloss
-            mx_month = row[0]
-        if percent_gainloss < greatest_decrease_gainloss:
-            greatest_decrease_gainloss = percent_gainloss
-            mn_month = row[0]
+#         # conditionals
+#         if percent_gainloss > greatest_increase_gainloss:
+#             greatest_increase_gainloss = percent_gainloss
+#             mx_month = row[0]
+#         if percent_gainloss < greatest_decrease_gainloss:
+#             greatest_decrease_gainloss = percent_gainloss
+#             mn_month = row[0]
 
-# time to get the average of changes in gain/loss over the whole period
-theaverage = (sum(difference_list) / total_months)
+# # time to get the average of changes in gain/loss over the whole period
+# theaverage = (sum(difference_list) / total_months)
 
-# time to print results to match view needed
-print("Financial Analysis")
-print(f"Total Months: {total_months}")
-print(f"Total: {total_gainloss}")
-print(f"Average Change: {theaverage}")
-print(f"Greatest Increase in Profits: {greatest_increase_gainloss}")
-print(f"Greatest Decrease in Profits: {greatest_decrease_gainloss}")
+# # time to print results to match view needed
+# print(f"Financial Analysis")
+# print(f"Total Months: {total_months}")
+# print(f"Total: {total_gainloss}")
+# print(f"Average Change: {theaverage}")
+# print(f"Greatest Increase in Profits: {greatest_increase_gainloss}")
+# print(f"Greatest Decrease in Profits: {greatest_decrease_gainloss}")
 
-# creating export file
-output = open("results.txt","w+")
+# # creating export file and writing to file results
+# output = open("results.txt","w+")
 
-output.write("Financial Analysis \n")
-output.write("-------------------------------------------- \n")
-output.write(f"Total Months: {total_months} \n")
-output.write(f"Total: {total_gainloss} \n")
-output.write(f"Average Change: {theaverage} \n")
-output.write(f"Greatest Increase in Profits: {greatest_increase_gainloss} \n")
-output.write(f"Greatest Decrease in Profits: {greatest_decrease_gainloss} \n")
+# output.write("Financial Analysis \n")
+# output.write("-------------------------------------------- \n")
+# output.write(f"Total Months: {total_months} \n")
+# output.write(f"Total: {total_gainloss} \n")
+# output.write(f"Average Change: {theaverage} \n")
+# output.write(f"Greatest Increase in Profits: {greatest_increase_gainloss} \n")
+# output.write(f"Greatest Decrease in Profits: {greatest_decrease_gainloss} \n")
 
