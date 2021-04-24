47de62f1338bd768c8d600a66d27a271facb4e5f
dalismo <agonz1975@gmail.com>
2021-04-24 01:53:25 -0400

    add

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,7 +13,11 @@ election_csv = os.path.join('.','Resources','election_data.csv')
 # Open and read the file using module
 with open(election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
-    csv_header = next(csvfile)
+    csv_header = next(csvreader)
 
-# start of the loop
     for row in csvreader:
+        total_votes = total_votes +1
+        candidates_name = (row[2])
+        if candidates_name in candidates:
+            candidate_place = candidates.index(candidates_name)
+            vote_count[candidate_place] = vote_count[candidate_place] +1
