f8627a12541dbab0b32ea419a767cef95a7731a8
dalismo <agonz1975@gmail.com>
2021-04-24 19:23:29 -0400

    This is tough but getting through it

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,9 +12,53 @@ Winner = 0
 WinnersName = ""
 i = 0
 
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
 
+	
+    for row in csvreader:
+        rowcount = rowcount + 1
+    print(rowcount)
+	    if row[2] not in Candidates:
+			Candidates.append(row[2])
+# 			VoteCount.append(0)
+# 	    else:
+# 			VoteCount[Candidates.index(row[2])] = VoteCount[Candidates.index(row[2])] + 1
 
+# Winner = max(range(len(VoteCount)), key = lambda x: VoteCount[x])
+# WinnersName = Candidates[int(Winner)]
+
+print("Election Results are in!")
+print("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
+print("Total Votes: " + str(rowcount))
+print("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
     
+print("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
+# print("Winner: " + str(WinnersName))
+print("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
+
+# i = 0
+
+# with open("election_results.txt", "w") as output:
+# 	output.write("Election Results are in!\n")
+# 	output.write("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-\n")
+# 	output.write("Total Votes: " + str(rowcount) + "\n")
+# 	output.write("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
