6870ff1a61fef2e70b893378d5b75c460df06725
dalismo <agonz1975@gmail.com>
2021-04-24 02:24:33 -0400

    add

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,8 +4,11 @@ import csv
 
 # variables to hold datapoints
 total_votes = 0
-candidates = []
-vote_count= []
+candidates = ""
+candidates_votes = {}
+candidates_percent = {}
+winning_votes = 0
+winner = ""
 
 # filepath 
 election_csv = os.path.join('.','Resources','election_data.csv')
@@ -13,15 +16,17 @@ election_csv = os.path.join('.','Resources','election_data.csv')
 # Open and read the file using module
 with open(election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
-    csv_header = next(csvreader)
+    next(csvreader)
 
     for row in csvreader:
         total_votes = total_votes + 1
-        # column 3 location
-        candidates_name = (row[2])
-        if candidates_name in candidates:
+        candidates = row[2]
+        if candidates in candidates_votes:
             candidate_place = candidates.index(candidates_name)
-            vote_count[candidate_place] = vote_count[candidate_place] +1
+            condidate_votes[candidates] = condidate_votes[candidates] +1
         else:
-            candidates.append(candidates_name)
-            vote_count
+            candidates_votes[condidates] = 1
+
+# what is the vote percentage 
+
+
