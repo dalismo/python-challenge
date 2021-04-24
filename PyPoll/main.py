51de6527df31ad53faf896d5ac4f82f5d26ed36c
dalismo <agonz1975@gmail.com>
2021-04-24 03:09:20 -0400

    add

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,39 +1,21 @@
-# modules to bring in
 import os
 import csv
 
-# variables to hold datapoints
-total_votes = 0 
-candidate_vtd = {}
-candidate_votes = 0
-candiate_vtpercent={}
-winner = ""
-
-
-# filepath 
+# Import CSV here
 election_csv = os.path.join('.','Resources','election_data.csv')
+print(election_csv)
 
-# Open and read the file using module
-with open(election_csv, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    csv_header = next(csvreader)
+Candidates = []
+VoteCount = []
+rowcount = 0
+Winner = 0
+WinnersName = ""
+i = 0
 
-    for row in csvreader:
-        if row[2] in candidate_vtd:
-            candidate_vtd[row[2]] += 1
-        else:
-            candidate_vtd[row[2]] = 1
-        total_votes = total_votes + 1
-        winner = max(candidate_vtd, key=candidate_vtd.get)
 
-vote_percentdict = {}
-for key in candidate_vtd.keys():
-    vote_list = []
-    vote_list.append(candidate_vtd[key]/total_votes*100)
-    vote_list.append(candidate_vtd[key])
-    vote_percentdict[key] = vote_list
 
     
+
 
 
 
