766c7c64240b6147b43a48895eec9e1cd620685e
dalismo <agonz1975@gmail.com>
2021-04-24 01:32:53 -0400

    add

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,11 @@
 import os
 import csv
 
+# variables to hold datapoints
+total_votes = 0
+candidates = []
+vote_count= []
+
 # filepath 
 election_csv = os.path.join('.','Resources','election_data.csv')
 
@@ -10,16 +15,7 @@ with open(election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvfile)
 
-#take the first row
-firstrow = next(csvreader)
-
-# vartiables to hold datapoints
-total_cast = 0
-candidates = {}
-percent = {}
-thewinner = ""
-win_count = 0
-
 # start of the loop
-for row in csvreader:
-    total_cast = total_cast +1
+    for row in csvreader:
+
+
