4476c9cb29f1fb87d46f3d6bef430c7b3f434dbb
dalismo <agonz1975@gmail.com>
2021-04-24 02:56:06 -0400

    add

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,33 +3,37 @@ import os
 import csv
 
 # variables to hold datapoints
-candidates = []
 total_votes = 0 
-vote_counts = []
-election_data = ['1', '2']
+candidate_vtd = {}
+candidate_votes = 0
+candiate_vtpercent={}
+winner = ""
 
 
 # filepath 
 election_csv = os.path.join('.','Resources','election_data.csv')
 
 # Open and read the file using module
-with open(election_csv) as csvfile:
+with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
 
     for row in csvreader:
+        if row[2] in candidate_vtd:
+            candidate_vtd[row[2]] += 1
+        else:
+            candidate_vtd[row[2]] = 1
         total_votes = total_votes + 1
-# print(total_votes)
-        candidate = row[2]
-        print(candidate)
+        winner = max(candidate_vtd, key=candidate_vtd.get)
 
-# # what is the vote percentage 
-# for person, vote_count in candidates_votes.items():
-#     candidates_percent[person] = '{0:.0%}'.format(vote_count / total_votes)
-#     if vote_count > winner_votes:
-#         winner_votes = vote_count
-#         winner = person
-# print(total_votes)
+vote_percentdict = {}
+for key in candidate_vtd.keys():
+    vote_list = []
+    vote_list.append(candidate_vtd[key]/total_votes*100)
+    vote_list.append(candidate_vtd[key])
+    vote_percentdict[key] = vote_list
+
+    
 
 
 
